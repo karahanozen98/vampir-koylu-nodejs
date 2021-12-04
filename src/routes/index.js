3b11e9b4b5ddcad87706e8d82b02937fe3a97f26
Karahan <karahanozen98@gmail.com>
2021-12-04 17:59:04 +0300

    Apı welcome page added

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ const AsyncRouter = require("../utils/asyncRouter");
 const router = new AsyncRouter(express.Router());
 const game = require("../Game");
 
+router.get("/", (_req, res) => {
+  res.send("API is up and running");
+});
+
 router.get("/users", (_req, res) => {
     res.send(game.users);
 });
